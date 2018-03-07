1b268e275588ab559559a90527fc81056435decb
lwesy <lw@esyoil.com>
2018-03-07 14:33:07 +0100

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
 import './scss/style.scss'
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 console.log('it works!')
